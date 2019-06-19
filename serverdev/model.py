25913407f6ba2eb1165eba0a80b8cc0566f23ff8
Pyle <caiden.pyle@netapp.com>
2019-06-19 12:28:52 -0500

    Added docstrings to functions and classes

--- serverdev/model.py
+++ serverdev/model.py
@@ -49,6 +49,8 @@ def stdout(host, username, password, command):
 
 
 def memoize(function):
+    """Memoization decorator.
+    """
     cache = dict()
 
     def memoized_function(*args):
@@ -62,27 +64,53 @@ def memoize(function):
 
 @memoize
 def get_all_vms():
+    """Get a list of all virtual machine dictionaries that are managed by
+    the current vCenter session. This function is memoized.
+    
+    :return list[dict[str, str]]: list of json dictionaries of virtual
+                                  machines.
+    """
     vms = session.get(entry_url+'/vcenter/vm')
     return json.loads(vms.text)['value']
 
 
 @memoize
 def get_all_vmids():
+    """Get the vCenter REST api vmid name for each virtual machine. This
+    function is memoized.
+    
+    :return list[str]: each virtual machine's vmid.
+    """
     return [vm['vm'] for vm in get_all_vms()]
 
 
 @memoize
 def get_all_hosts():
+    """Get a list of all esx servers managed by the current vCenter session.
+    This function is memoized.
+    
+    :return list[dict[str, str]]: list of host json dictionaries.
+    """
     hosts = session.get(entry_url+'/vcenter/host')
     return json.loads(hosts.text)['value']
 
 
 def get_all_host_ips():
+    """Get the IP address of each esx server managed by the current vCenter
+    session.
+    
+    :return list[str]: list of host IP addresses.
+    """
     hosts = get_all_hosts()
     return [host['name'] for host in hosts]
 
 
 def get_vm_by_hostname(vm_name):
+    """Get a virtual machine's json dictionary by its hostname.
+    
+    :param str vm_name: hostname of virtual machine to return.
+    :return dict[str, str]: json dictionary of virtual machine.
+    """
     for vm in get_all_vms():
         if vm['name'] == vm_name:
             return vm
@@ -90,6 +118,11 @@ def get_vm_by_hostname(vm_name):
 
 
 def get_host_by_ip(ip):
+    """Get an esx host's json dictionary by its IP address.
+    
+    :param str ip: IP address of host to return.
+    :return dict[str, str]: json dictionary of esx host.
+    """
     for host in get_all_hosts():
         if host['name'] == ip:
             return host
@@ -97,6 +130,10 @@ def get_host_by_ip(ip):
 
 
 class VCenterSession(object):
+    """Create a vCenter session and authenticate to the vCenter server. This
+    class must be instantiated for all other methods and functions to make
+    REST api calls.
+    """
     def __init__(self, address, username, password):
         self.setup(address)
         self.username = username
@@ -114,6 +151,8 @@ class VCenterSession(object):
 
 
 class EsxHost(object):
+    """Representation of an esx server.
+    """
     def __init__(self, address, username, password):
         self.address = address
         self.username = username
@@ -121,20 +160,41 @@ class EsxHost(object):
         self.url = entry_url + "/vcenter/host/{}".format(self.address)
         
     def _get_vmids(self):
+        """Get a list of vmids that are specific to the local esx operating
+        system.
+        
+        :return list[str]: list of vmids for all virtual machines.
+        """
         command = self.execute('vim-cmd vmsvc/getallvms')
         return [x.strip().split()[0] for x in command[1:]]
 
     def execute(self, command):
+        """Execute a command over ssh on the esx server and return stdout.
+        
+        :return list[str]: lines of stdout from command execution.
+        """
         return stdout(self.address, self.username, self.password, command)
 
     def list_vm_hostnames(self):
+        """Get the hostname for each virtual machine that resides on
+        the esx server.
+        
+        :return list[str]: list of virtual machine hostnames.
+        """
         command = self.execute('vim-cmd vmsvc/getallvms')
         return [x.strip().split()[1] for x in command[1:]]
 
-    def list_vm_vmids(self):
+    def list_vm_api_names(self):
+        """Get a list of virtual machine REST api names for making api calls.
+        
+        :return list[str]: virtual machine REST api names.
+        """
         return [get_vm_by_hostname(x)['vm'] for x in self.list_vm_hostnames()]
 
     def list_vm_ips(self):
+        """Get a list of virtual machine IP addresses. Virtual machines will
+        only return an IP address if their power state is on.
+        """
         rtn = list()
         for vmid in self._get_vmids():
             pre = "vim-cmd vmsvc/get.guest "
@@ -143,21 +203,38 @@ class EsxHost(object):
         return rtn
 
     def enter_maint_mode(self):
+        """Put this esx server into maintenance mode.
+        """
         self.execute('esxcli system maintenanceMode set --enable true')
 
     def exit_maint_mode(self):
+        """Exit this esx server from maintenance mode.
+        """
         self.execute('esxcli system maintenanceMode set --enable false')
 
     def reboot(self):
+        """Reboot this esx server.
+        """
         self.execute('reboot')
 
     @memoize
     def get_vms(self):
-        return [Vm(vmid) for vmid in self.list_vm_vmids()]
+        """Get a list of instantiated Vm objects for each virtual machine on
+        this esx server. This method is memoized.
+        
+        :return list[serverdev.model.Vm]: list of instantiated virtual
+                                          machines.
+        """
+        return [Vm(vmid) for vmid in self.list_vm_api_names()]
 
 
 class Vm(object):
+    """Representation of virtual machine.
+    """
     def __init__(self, vmid):
+        """
+        :param str vmid: REST api name for this virtual machine.
+        """
         if vmid not in get_all_vmids():
             error = "{} is not a valid virtual machind."
             raise InvalidVirtualMachine(error.format(self.vmid))
@@ -165,19 +242,32 @@ class Vm(object):
         self.url = entry_url + '/vcenter/vm/' + vmid
 
     def poweroff(self):
+        """Turn off a virtual machine.
+        """
         print(self.url+'/power/stop')
         session.post(self.url+'/power/stop')
 
     def poweron(self):
+        """Turn on a virtual machine.
+        """
         print(self.url+'/power/start')
         session.post(self.url+'/power/start')
 
     @memoize
     def get_disk_names(self):
+        """Get the REST api disk names for all drives on a virtual machine.
+        This function is memoized.
+        
+        :return list[str]: list of virtual disk names.
+        """
         disks = session.get(self.url+'/hardware/disk')
         return [x['disk'] for x in json.loads(disks.text)['value']]
 
     def get_disks(self):
+        """Get a list of a virtual machine disk's json dictionaries.
+        
+        :return list[dict[str, str]]: list of disk json dictionaries.
+        """
         rtn = list()
         for disk in self.get_disk_names():
             disk = json.loads(session.get(self.url+'/hardware/disk/'+disk).text)
@@ -185,12 +275,21 @@ class Vm(object):
         return rtn
 
     def unmap_disk(self, disk):
+        """Unmap a disk from a virtual machine by its REST api disk name. Disk
+        files are note deleted, just removed from virtual machine configuration.
+        
+        :param str disk: disk to unmap from virtual machine.
+        """
         if disk not in self.get_disk_names():
             error = "{} is not a valid disk for this virtual machine."
             raise InvalidVirtualDisk(error.format(disk))
         session.delete(self.url+'/hardware/disk/'+disk)
 
     def unmap_all_disks(self):
+        """Unmap all virtual disks from a virtual machine except for the boot
+        disk. Disk files are not deleted, just removed from virtual machine
+        configuration.
+        """
         disks = self.get_disk_names()
         if len(disks) > 1:
             for disk in disks[1:]:
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
     name='server-dev',
 
-    version='1.0.5',
+    version='1.0.6',
 
     description='Server administration tools',
 
